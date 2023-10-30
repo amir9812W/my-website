745d6cbeee7039239c720ab60387361657ee8c67
amir9812W <122056621+amir9812W@users.noreply.github.com>
2023-10-30 09:35:57 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,61 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js'
+
+import { getDatabase, ref ,push, onValue, remove } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js'
+
+const appSettings = {
+  databaseURL : "https://project-2-b0f21-default-rtdb.firebaseio.com/"
+}
+
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const refrence = ref(database, 'movies')
+const refrence2 = ref(database, 'toDos')
+
+const button = document.querySelector('.input2')
+const nani = document.querySelector('.type2')
+const list = document.querySelector('.who')
+
+console.log('wtf is wrong with this fucking piece of shit')
+
+onValue(refrence2, function(snapshot) {
+  if(snapshot.exists()){
+    let arr = Object.entries(snapshot.val())
+    list.innerHTML = ''
+
+
+    for(let i = 0 ; i < arr.length ; i++){
+
+      let current = arr[i]
+      let ids = current[0]
+      let keys = current[1]
+
+  
+      let newEl = document.createElement('li')
+      newEl.textContent = keys
+      list.append(newEl)
+  
+      newEl.addEventListener('click', function() {
+        let exactLocation = ref(database, `toDos/${ids}`)
+        remove(exactLocation)
+      })
+    }
+  }else{
+    list.innerHTML += 'no items yet...'
+  }
+
+})
+
+
+document.querySelector('body').addEventListener('keydown', function(event) {
+  if(event.key === 'Enter'){
+    let data3 = button.value
+    push(refrence2, data3)
+  }
+})
+
+
+nani.addEventListener('click', function() {
+  let data3 = button.value
+  push(refrence2, data3)
+})
