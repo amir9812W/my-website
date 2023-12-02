f5f5877891e755ca1d9942a614aa15838099f53e
amir9812W <122056621+amir9812W@users.noreply.github.com>
2023-12-02 08:58:34 +0330

    Update index.js

--- index.js
+++ index.js
@@ -1,58 +1,50 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import { getDatabase, ref, push, onValue, remove } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js';
 
-import { getDatabase, ref ,push, onValue, remove } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js'
+const firebaseConfig = {
+  databaseURL: "https://project-2-b0f21-default-rtdb.firebaseio.com",
+};
 
- const firebaseConfig = {
-    databaseURL: "https://project-2-b0f21-default-rtdb.firebaseio.com",
-  };
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+const reference = ref(database, 'movies'); // Corrected variable name
 
-const app = initializeApp(firebaseConfig)
-const database = getDatabase(app)
-const refrence = ref(database, 'movies')
+const button = document.querySelector('.input2');
+const nani = document.querySelector('.type2');
+const list = document.querySelector('.who');
 
+onValue(reference, function(snapshot) { // Corrected variable name
+  if (snapshot.exists()) {
+    let arr = Object.entries(snapshot.val());
+    list.innerHTML = '';
 
-const button = document.querySelector('.input2')
-const nani = document.querySelector('.type2')
-const list = document.querySelector('.who')
+    for (let i = 0; i < arr.length; i++) {
+      let current = arr[i];
+      let ids = current[0];
+      let keys = current[1];
 
-onValue(refrence2, function(snapshot) {
-  if(snapshot.exists()){
-    let arr = Object.entries(snapshot.val())
-    list.innerHTML = ''
+      let newEl = document.createElement('li');
+      newEl.textContent = keys;
+      list.append(newEl);
 
-
-    for(let i = 0 ; i < arr.length ; i++){
-
-      let current = arr[i]
-      let ids = current[0]
-      let keys = current[1]
-
-  
-      let newEl = document.createElement('li')
-      newEl.textContent = keys
-      list.append(newEl)
-  
       newEl.addEventListener('click', function() {
-        let exactLocation = ref(database, `toDos/${ids}`)
-        remove(exactLocation)
-      })
+        let exactLocation = ref(database, `toDos/${ids}`);
+        remove(exactLocation);
+      });
     }
-  }else{
-    list.innerHTML += 'no items yet...'
+  } else {
+    list.innerHTML += 'no items yet...';
   }
-
-})
-
+});
 
 document.querySelector('body').addEventListener('keydown', function(event) {
-  if(event.key === 'Enter'){
-    let data3 = button.value
-    push(refrence, data3)
+  if (event.key === 'Enter') {
+    let data3 = button.value;
+    push(reference, data3); // Corrected variable name
   }
-})
-
+});
 
 nani.addEventListener('click', function() {
-  let data3 = button.value
-  push(refrence, data3)
-})
+  let data3 = button.value;
+  push(reference, data3); // Corrected variable name
+});
