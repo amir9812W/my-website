1be1c885f07096dbcc2681e6a455cc2a81088ab4
amir9812W <122056621+amir9812W@users.noreply.github.com>
2023-12-02 08:49:43 +0330

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js"
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
 
 import { getDatabase, ref ,push, onValue, remove } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js'
 
